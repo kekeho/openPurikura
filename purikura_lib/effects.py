b941bc3c30270f4960c54f5667552beb2a2109ff
Hiroki.T <hirodora@me.com>
2018-10-07 16:01:16 +0900

    add: chin befautify function fix: autopep8

--- purikura_lib/effects.py
+++ purikura_lib/effects.py
@@ -13,6 +13,7 @@ sys.path.append(CURRENT_DIRNAME + '/')
 import utils
 import find
 
+
 class MaskShapeError(Exception):
     def __init__(self, ndarray1: np.ndarray, ndarray2: np.ndarray):
         self.shape1 = ndarray1.shape
@@ -183,12 +184,15 @@ def eyes_shape_beautify(image: np.ndarray, face_landmarks: list):
         l_upside_to_points = []
         l_down_from_points = []
         l_down_to_points = []
-        for i, j in zip(range(134,143+1), list(range(143, 153+1))[:][::-1]):
-            l_upside_from_points.append(landmark[i] + (landmark[j] - landmark[i]) / 50)
-            l_upside_to_points.append(landmark[i] - (landmark[j] - landmark[i]) / 5)
-            l_down_from_points.append(landmark[j] - (landmark[j] - landmark[i]) / 20)
-            l_down_to_points.append(landmark[j] + (landmark[j] - landmark[i]) / 5)
-
+        for i, j in zip(range(134, 143 + 1), list(range(144, 153 + 1))[:][::-1]):
+            l_upside_from_points.append(
+                landmark[i] + (landmark[j] - landmark[i]) / 50)
+            l_upside_to_points.append(
+                landmark[i] - (landmark[j] - landmark[i]) / 5)
+            l_down_from_points.append(
+                landmark[j] - (landmark[j] - landmark[i]) / 20)
+            l_down_to_points.append(
+                landmark[j] + (landmark[j] - landmark[i]) / 5)
 
         image = distort(image, l_upside_from_points, l_upside_to_points,
                         [(x1, y1), (x2, y1), (x2, y2),
@@ -203,12 +207,15 @@ def eyes_shape_beautify(image: np.ndarray, face_landmarks: list):
         r_upside_to_points = []
         r_down_from_points = []
         r_down_to_points = []
-        for i, j in zip(range(114,123+1), list(range(124, 133+1))[:][::-1]):
-            r_upside_from_points.append(landmark[i] + (landmark[j] - landmark[i]) / 50)
-            r_upside_to_points.append(landmark[i] - (landmark[j] - landmark[i]) / 5)
-            r_down_from_points.append(landmark[j] - (landmark[j] - landmark[i]) / 20)
-            r_down_to_points.append(landmark[j] + (landmark[j] - landmark[i]) / 5)
-
+        for i, j in zip(range(114, 123 + 1), list(range(124, 133 + 1))[:][::-1]):
+            r_upside_from_points.append(
+                landmark[i] + (landmark[j] - landmark[i]) / 50)
+            r_upside_to_points.append(
+                landmark[i] - (landmark[j] - landmark[i]) / 5)
+            r_down_from_points.append(
+                landmark[j] - (landmark[j] - landmark[i]) / 20)
+            r_down_to_points.append(
+                landmark[j] + (landmark[j] - landmark[i]) / 5)
 
         image = distort(image, r_upside_from_points, r_upside_to_points,
                         [(x1, y1), (x2, y1), (x2, y2),
@@ -217,8 +224,10 @@ def eyes_shape_beautify(image: np.ndarray, face_landmarks: list):
                         [(x1, y1), (x2, y1), (x2, y2),
                             (x1, y2)])
 
-        highlight = cv2.imread(CURRENT_DIRNAME + '/eyes_highlight.png', cv2.IMREAD_UNCHANGED)
-        
+        # add highlight
+        highlight = cv2.imread(
+            CURRENT_DIRNAME + '/eyes_highlight.png', cv2.IMREAD_UNCHANGED)
+
         line1 = Segment(Point(landmark[134]), Point(landmark[145]))
         line2 = Segment(Point(landmark[139]), Point(landmark[150]))
         ls = intersection(line1, line2)
@@ -226,22 +235,48 @@ def eyes_shape_beautify(image: np.ndarray, face_landmarks: list):
         dx, dy = (landmark[150] - landmark[139]) / 5
         x, y = int(ls[0].x) - dx, int(ls[0].y) - dy
         w = np.linalg.norm(landmark[150] - landmark[139])
-        ratio = w/highlight.shape[1] * 100
-        x = int(x - highlight.shape[0]*(ratio/100)/2)
-        y = int(y - highlight.shape[1]*(ratio/100)/2)
+        ratio = w / highlight.shape[1] * 100
+        x = int(x - highlight.shape[0] * (ratio / 100) / 2)
+        y = int(y - highlight.shape[1] * (ratio / 100) / 2)
         image = merge(image, highlight, x, y, ratio)
 
         line1 = Segment(Point(landmark[114]), Point(landmark[124]))
         line2 = Segment(Point(landmark[120]), Point(landmark[129]))
         ls = intersection(line1, line2)
-        
+
         dx, dy = (landmark[129] - landmark[120]) / 5
         x, y = int(ls[0].x) - dx, int(ls[0].y) - dy
         w = np.linalg.norm(landmark[129] - landmark[120])
-        ratio = w/highlight.shape[1] * 100
-        x = int(x - highlight.shape[0]*(ratio/100)/2)
-        y = int(y - highlight.shape[1]*(ratio/100)/2)
+        ratio = w / highlight.shape[1] * 100
+        x = int(x - highlight.shape[0] * (ratio / 100) / 2)
+        y = int(y - highlight.shape[1] * (ratio / 100) / 2)
         image = merge(image, highlight, x, y, ratio)
+
+    return image
+
+
+def chin_shape_beautify(image: np.ndarray, face_landmarks: list):
+    """Beautify chin💁
+    This function can uses for many people
+    """
+    for landmark in face_landmarks:
+        x1, y1, x2, y2 = utils.detect_roi(landmark[0:40 + 1])  # roi
+        l_from_points = []
+        l_to_points = []
+        r_from_points = []
+        r_to_points = []
+        for i, j in zip(range(5, 13 + 1), list(range(27, 35 + 1))[:][::-1]):
+            l_from_points.append(landmark[i])
+            l_to_points.append(landmark[i] + (landmark[j] - landmark[i]) / 23)
+            r_from_points.append(landmark[j])
+            r_to_points.append(landmark[j] - (landmark[j] - landmark[i]) / 23)
+
+        image = distort(image, l_from_points, l_to_points,
+                        [(x1, y1), (x2, y1), (x2, y2),
+                            (x1, y2)])
+        image = distort(image, r_from_points, r_to_points,
+                        [(x1, y1), (x2, y1), (x2, y2),
+                            (x1, y2)])
 
     return image
 
@@ -255,45 +290,57 @@ def animal_ears(image: np.ndarray, ear_image: PngImageFile, face_landmarks: list
     """
     for landmark in face_landmarks:
         # position of ear_image center
-        left = landmark[140] - ((landmark[20] - landmark[140])*0.8).astype(np.int32)
-        right = landmark[119] - ((landmark[20] - landmark[119])*0.8).astype(np.int32)
-        
-        
+        left = landmark[140] - \
+            ((landmark[20] - landmark[140]) * 0.8).astype(np.int32)
+        right = landmark[119] - \
+            ((landmark[20] - landmark[119]) * 0.8).astype(np.int32)
+
         l_delta = landmark[20] - landmark[140]
         l_rad = math.atan2(l_delta[1], l_delta[0])
         l_deg = math.degrees(l_rad)
-        l_deg = (l_deg-90) * -1
+        l_deg = (l_deg - 90) * -1
         l_ear_image = ear_image.rotate(l_deg)
         l_ear_image = np.asarray(l_ear_image)
 
         r_delta = landmark[20] - landmark[119]
         r_rad = math.atan2(r_delta[1], r_delta[0])
         r_deg = math.degrees(r_rad)
-        r_deg = (r_deg-90) * -1
+        r_deg = (r_deg - 90) * -1
         r_ear_image = ear_image.rotate(r_deg)
         r_ear_image = np.asarray(r_ear_image)
 
-        ear_par = 20 #resize 20%
-        image = merge(image, l_ear_image, int(left[0]-l_ear_image.shape[0]/2*(ear_par/100)), int(left[1]-l_ear_image.shape[1]/2*(ear_par/100)), per=20)
-        image = merge(image, r_ear_image, int(right[0]-r_ear_image.shape[0]/2*(ear_par/100)), int(right[1]-r_ear_image.shape[1]/2*(ear_par/100)), per=20)
+        ear_par = 20  # resize 20%
+        image = merge(image, l_ear_image, int(left[0] - l_ear_image.shape[0] / 2 * (
+            ear_par / 100)), int(left[1] - l_ear_image.shape[1] / 2 * (ear_par / 100)), per=20)
+        image = merge(image, r_ear_image, int(right[0] - r_ear_image.shape[0] / 2 * (
+            ear_par / 100)), int(right[1] - r_ear_image.shape[1] / 2 * (ear_par / 100)), per=20)
 
     return image
 
 
 def main():
-    image = cv2.imread(CURRENT_DIRNAME + '/../Tests/sources/japanese_girl.jpg')
-    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    face_landmarks = find.facemark(gray_img)
-
-    image = nose_shape_beautify(image, face_landmarks)
-    image = eyes_shape_beautify(image, face_landmarks)
-    image = skin_beautify(image, rate=5)
-
     nekomimi = Image.open(CURRENT_DIRNAME + '/../Tests/sources/nekomimi.png')
-    image = animal_ears(image, nekomimi, face_landmarks)
 
-    cv2.imshow('image', image)
-    cv2.waitKey()
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        # ret, image = cap.read()
+        image = cv2.imread(CURRENT_DIRNAME +
+                           '/../Tests/sources/katy.jpg')
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        face_landmarks = find.facemark(gray_img)
+
+        image = nose_shape_beautify(image, face_landmarks)
+        image = eyes_shape_beautify(image, face_landmarks)
+        image = chin_shape_beautify(image, face_landmarks)
+        image = skin_beautify(image, rate=5)
+
+        image = animal_ears(image, nekomimi, face_landmarks)
+
+        cv2.imshow('image', image)
+        cv2.waitKey()
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
