2e14afef474e92ae4035b2fc648ce2676d7f6c1b
Hg <16424@g.nagano-nct.ac.jp>
2018-10-26 13:39:30 +0900

    update: js
    何を更新したのか忘れた

--- templates/assets/js/draw_log.js
+++ templates/assets/js/draw_log.js
@@ -1,55 +1,73 @@
 class Log {
   constructor(canvas){
-    this.canvas = canvas;
+    this.target = canvas;
     this.current = 0;
     this.log     = [];
   }
 
-  /* add log */
+  // add log
   add(){
-    /* create a CANVAS for log */
+    // create a <canvas> for log
     let canvas_log = document.createElement("canvas");
-    canvas_log.width  = this.canvas.width;
-    canvas_log.height = this.canvas.height;
+    canvas_log.width  = this.target.width;
+    canvas_log.height = this.target.height;
     let ctx_log = canvas_log.getContext("2d");
 
-    /* copy to canvas_log */
-    ctx_log.drawImage(this.canvas, 0, 0);
+    // copy to canvas_log
+    ctx_log.drawImage(this.target, 0, 0);
     this.current++;
     this.log[this.current];
 
-    /* clear log */
+    // clear log
     if (current != this.log.length - 1) {
       this.log.length = this.current + 1;
     }
   }
 
-  /* check that this can redo */
+  /*
+  // check that taeget can redo
   can_redo(){
     (this.current < this.log.length - 1) ? true : false;
   }
 
-  /* check that this can undo */
+  // check that taeget can undo
   can_undo(){
     (this.current > 0) ? true : false;
   }
+  */
 
-  /* redo */
+  // redo
   redo(){
     if (this.current < this.log.length - 1) {
       this.current++;
-      return log[current];
+
+      let ctx_target = this.target.getContext("2d");
+      ctx_target.globalCompositeOperation = "source-over";
+      ctx_target.clearRect(0, 0, this.target.width, this.target.height);
+      ctx_target.drawImage(this.log[this.current], 0, 0);
+
+      return true;
     }
-    return null;
+    return false;
   }
 
-  /* undo */
+  // undo
   undo(){
     if (this.current > 0) {
       this.current--;
-      return log[current];
+
+      let ctx_target = this.target.getContext("2d");
+      ctx_target.globalCompositeOperation = "source-over";
+      ctx_target.clearRect(0, 0, this.target.width, this.target.height);
+      ctx_target.drawImage(this.log[this.current], 0, 0);
+
+      return true;
     }
-    return null;
+    return false;
+  }
+
+  // return current image
+  image(){
+    return this.log[this.current];
   }
 }
-
