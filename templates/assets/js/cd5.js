6615c28ef25681add7192f1907a87d6b62a95bf0
Daichi Fukuzawa <daichimax78@gmila.com>
2018-10-30 11:29:05 +0900

    add: カウントダウン音 (ピッ)

--- templates/assets/js/cd5.js
+++ templates/assets/js/cd5.js
@@ -1,29 +1,34 @@
-var shutter = new Audio('./assets/sounds/shutter.mp3');
-
 $(function(){
-  let time = 6;
+  let shutter = new Audio("./assets/sounds/shutter.mp3");
+  let countdown = new Audio("./assets/sounds/countdown.mp3");
 
-  setInterval(function() {
+  let time = 5;
+
+  let interval = setInterval(function() {
+    $(".timer").text(time);
+
     if (time > 0) {
-      time--;
-      $('.timer').text(time);
+      countdown.play();
 
-      if (time == 0) {
-        shutter.play()
-        $('#video').attr('src', './assets/src/white.png');
-        $('#teacher').hide();
+    } else if (time == 0) {
+      shutter.play();
+      $("#video").attr("src", "./assets/src/white.png");
+      $("#teacher").hide();
 
-        $.ajax({
-          type: 'POST',
-          url: '/take',
-          success: function() {
-            $('#video').attr('src', './assets/photos/c' + cache_num + '_before-' + take_num + '.png');
-            setTimeout(function() {
-              location.reload();
-            }, 3000);
-          }
-        });
-      }
+      $.ajax({
+        type: "POST",
+        url: "/take",
+        success: function() {
+          $("#video").attr("src", "./assets/photos/c" + cache_num + "_before-" + take_num + ".png");
+          setTimeout(function() {
+            location.reload();
+          }, 3000);
+        }
+      });
+
+      clearInterval(interval);
     }
+
+    time--;
   }, 1300);
 });
